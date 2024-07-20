9f1f4a6f5818852e7a29024533d03263e100b9e1
Ell Bradshaw <ell@c9a.co>
2024-07-19 17:08:43 -0700

    Draw sampling points

--- src/draw.rs
+++ src/draw.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max, min};
 use std::path::PathBuf;
 use imageproc::drawing;
 use imageproc::rect::Rect;
@@ -20,17 +21,40 @@ pub fn draw_debug(dir: &PathBuf, out_dir: &PathBuf) {
             let bounds = crop_boundaries(&gray, 0.05);
             let points = grid_points(&bounds, 10);
             let mut out_gray = imageproc::image::GrayImage::from_raw(image.width(), image.height(), gray.into_iter().flatten().collect()).expect("Ahh");
+
+            let width = bounds.upper_x - bounds.lower_x;
+            let height = bounds.upper_y - bounds.lower_y;
+
             drawing::draw_hollow_rect_mut(&mut out_gray,
                 Rect::at(bounds.lower_x as i32, bounds.lower_y as i32)
-                    .of_size((bounds.upper_x - bounds.lower_x) as u32, (bounds.upper_y - bounds.lower_y) as u32),
+                    .of_size(width as u32, height as u32),
                 imageproc::image::Luma([255])
             );
             drawing::draw_hollow_rect_mut(&mut out_gray,
                 Rect::at((bounds.lower_x+1) as i32, (bounds.lower_y-1) as i32)
-                    .of_size((bounds.upper_x - bounds.lower_x-2) as u32, (bounds.upper_y - bounds.lower_y-2) as u32),
+                    .of_size((width-2) as u32, (height-2) as u32),
                 imageproc::image::Luma([0])
             );
-            println!("{}", &dest.display());
+
+            let square_edge = 
+        max(
+            2_usize,
+            ((0.5 + min(width, height) as f32 / 20.0) as f32).floor() as usize,
+        ) / 2;
+
+
+            for (_, point) in points {
+                drawing::draw_hollow_circle_mut(&mut out_gray,
+                    (point.0 as i32, point.1 as i32), 
+                    square_edge.try_into().unwrap(),
+                    imageproc::image::Luma([255])
+                );
+                drawing::draw_hollow_circle_mut(&mut out_gray,
+                    (point.0 as i32, point.1 as i32), 
+                    (square_edge-1).try_into().unwrap(),
+                    imageproc::image::Luma([0])
+                );
+            }
             out_gray.save(&dest.join(name)).expect("Coudln't save iamge");
         }
     }
