b2b0786516eb16bd7007f0bc53f5ccab72deb9ee
Ell Bradshaw <ell@c9a.co>
2024-07-23 10:44:49 -0700

    Start to add summary stats

--- grid.R
+++ grid.R
@@ -1,10 +1,14 @@
 library('tidyverse')
 library('ggridges')
-dists = read_csv('grid.csv')
+dists = rbind(
+  read_csv('grid.orig.csv') %>% mutate(ver="orig")
+  read_csv('grid.before.csv') %>% mutate(ver="before")
+  read_csv('grid.after.csv') %>% mutate(ver="after")
+)
 sq = ggplot(dists %>% arrange(left, right, left_path, right_path), aes(
    x=paste(left, left_path),
    y=paste(right, right_path),
-   fill=distance
+   fill=similarity
  )) +
   geom_raster() +
   theme(axis.text.x=element_text(angle=90))
@@ -22,10 +26,22 @@ reldists = dists %>%
     )
   )
 
-p = ggplot(reldists, aes(x=distance, y=left, color=rel, size=(left==right))) +
+p = ggplot(reldists %>% filter(version="orig"), aes(x=similarity, y=left, color=rel, size=(left==right))) +
   geom_density_ridges(data = function(x) { x %>% filter(!is_same_image) }, color="#444", fill="#ccc") +
   geom_point(data = function(x) { x %>% filter(is_same_image) }) +
   scale_size_manual(values=c(0.5,2), guide=F) +
   scale_color_brewer(type="qual", palette="Set1") +
   lims(x=c(-1.00001,1.00001)) +
   labs(y="", color="Relationship")
+
+compare = reldists %>%
+  group_by(left) %>%
+  mutate(max_other = max(ifelse(is_same_image, similarity, 0))) %>%
+  filter(is_same_image) %>%
+  summarize(sumdiff = ifelse(
+    min(similarity) > max_other,
+    sum(similarity - max_other),
+    sum(ifelse(similarity < max_other, similarity - max_other, 0))
+  )
+
+b = ggplot(compare, aes(x=sumdiff, y=version)) + geom_boxplot()
